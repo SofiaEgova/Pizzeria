354374bf311e888602a5fed37d1b6333c2889b5d
sofiakulymanova <sofiakulymanova@gmail.com>
2018-04-07 08:12:15 +0400

    Лабораторная 12

---
+++ PizzeriaService/Interfaces/IReportService.cs
@@ -0,0 +1,23 @@
+﻿using PizzeriaService.BindingModels;
+using PizzeriaService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaService.Interfaces
+{
+    public interface IReportService
+    {
+        void SavePizzaPrice(ReportBindingModel model);
+
+        List<FridgesLoadViewModel> GetFridgesLoad();
+
+        void SaveFridgesLoad(ReportBindingModel model);
+
+        List<VisitorOrderPizzasModel> GetVisitorOrderPizzas(ReportBindingModel model);
+
+        void SaveVisitorOrderPizzas(ReportBindingModel model);
+    }
+}
