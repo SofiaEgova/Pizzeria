a01618b635c00f1d568bc2242a3b236fceff17b3
sofiakulymanova <sofiakulymanova@gmail.com>
2018-06-18 12:44:41 +0400

    впф лаба 12

---
+++ PizzeriaWpf/FridgesLoadWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using PizzeriaService.BindingModels;
+using PizzeriaService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Unity;
+
+namespace PizzeriaWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для FridgesLoadWindow.xaml
+    /// </summary>
+    public partial class FridgesLoadWindow : Window
+    {
+        [Unity.Attributes.Dependency]
+        public IUnityContainer Container { get; set; }
+
+        private readonly IReportService service;
+
+        public FridgesLoadWindow(IReportService service)
+        {
+            InitializeComponent();
+            this.service = service;
+            Loaded += FridgesLoadWindow_Load;
+        }
+
+        private void buttonToExcel_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                Filter = "xls|*.xls|xlsx|*.xlsx"
+            };
+            if (sfd.ShowDialog() == true)
+            {
+                try
+                {
+                    service.SaveFridgesLoad(new ReportBindingModel
+                    {
+                        FileName = sfd.FileName
+                    });
+                    MessageBox.Show("Выполнено", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void FridgesLoadWindow_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var dict = service.GetFridgesLoad();
+                if (dict != null)
+                {
+                    dataGrid.Items.Clear();
+
+                    foreach (var elem in dict)
+                    {
+                        dataGrid.Items.Add(new object[] { elem.FridgeName, "", "" });
+                        foreach (var listElem in elem.Ingredients)
+                        {
+                            dataGrid.Items.Add(new object[] { "", listElem.Item1, listElem.Item2 });
+                        }
+                        dataGrid.Items.Add(new object[] { "Итого", "", elem.TotalCount });
+                        dataGrid.Items.Add(new object[] { });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ PizzeriaWpf/VisitorOrderPizzasWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Win32;
+using PizzeriaService.BindingModels;
+using PizzeriaService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Unity;
+using Unity.Attributes;
+
+namespace PizzeriaWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для VisitorOrderPizzasWindow.xaml
+    /// </summary>
+    public partial class VisitorOrderPizzasWindow : Window
+    {
+
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        private readonly IReportService service;
+
+        private bool _isReportViewerLoaded;
+
+        public VisitorOrderPizzasWindow(IReportService service)
+        {
+            InitializeComponent();
+            this.service = service;
+            _reportViewer.Load += ReportViewer_Load;
+        }
+
+        private void ReportViewer_Load(object sender, EventArgs e)
+        {
+            if (!_isReportViewerLoaded)
+            {
+                if (!_isReportViewerLoaded)
+                {
+                    Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new
+                    Microsoft.Reporting.WinForms.ReportDataSource();
+
+                    Microsoft.Reporting.WinForms.ReportDataSource source = new
+                        Microsoft.Reporting.WinForms.ReportDataSource("DataSetOrderPizzas");
+                    _reportViewer.LocalReport.DataSources.Add(source);
+                    _reportViewer.RefreshReport();
+                    _isReportViewerLoaded = true;
+                }
+            }
+        }
+
+        private void buttonMake_Click(object sender, RoutedEventArgs e)
+        {
+            if (dateTimePickerFrom.SelectedDate >= dateTimePickerTo.SelectedDate)
+            {
+                MessageBox.Show("Дата начала должна быть меньше даты окончания", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                Microsoft.Reporting.WinForms.ReportParameter parameter =
+                    new Microsoft.Reporting.WinForms.ReportParameter("ReportParameterPeriod",
+                                            "c " + dateTimePickerFrom.SelectedDate.ToString() +
+                                            " по " + dateTimePickerTo.SelectedDate.ToString());
+                _reportViewer.LocalReport.SetParameters(parameter);
+
+                var dataSource = service.GetVisitorOrderPizzas(new ReportBindingModel
+                {
+                    DateFrom = dateTimePickerFrom.SelectedDate,
+                    DateTo = dateTimePickerTo.SelectedDate
+                });
+                Microsoft.Reporting.WinForms.ReportDataSource source = new
+                    Microsoft.Reporting.WinForms.ReportDataSource("DataSetOrderPizzas", dataSource);
+                _reportViewer.LocalReport.DataSources.Add(source);
+
+                _reportViewer.RefreshReport();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private void buttonToPdf_Click(object sender, RoutedEventArgs e)
+        {
+            if (dateTimePickerFrom.SelectedDate >= dateTimePickerTo.SelectedDate)
+            {
+                MessageBox.Show("Дата начала должна быть меньше даты окончания", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                Filter = "pdf|*.pdf"
+            };
+            if (sfd.ShowDialog() == true)
+            {
+                try
+                {
+                    service.SaveVisitorOrderPizzas(new ReportBindingModel
+                    {
+                        FileName = sfd.FileName,
+                        DateFrom = dateTimePickerFrom.SelectedDate,
+                        DateTo = dateTimePickerTo.SelectedDate
+                    });
+                    MessageBox.Show("Выполнено", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
