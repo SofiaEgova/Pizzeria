1b5bf9c3dc315f4e74ea992915be8d31e44def58
sofiakulymanova <sofiakulymanova@gmail.com>
2018-06-19 22:20:55 +0400

    правки

--- PizzeriaRestApi/App_Start/UnityConfig.cs
+++ PizzeriaRestApi/App_Start/UnityConfig.cs
@@ -56,6 +56,7 @@ namespace PizzeriaRestApi
             container.RegisterType<IFridgeService, FridgeServiceBD>(new HierarchicalLifetimeManager());
             container.RegisterType<IMainService, MainServiceBD>(new HierarchicalLifetimeManager());
             container.RegisterType<IReportService, ReportServiceBD>(new HierarchicalLifetimeManager());
-        }
+            container.RegisterType<IMessageInfoService, MessageInfoServiceBD>(new HierarchicalLifetimeManager());
         }
     }
+}
--- PizzeriaView/Program.cs
+++ PizzeriaView/Program.cs
@@ -20,6 +20,7 @@ namespace PizzeriaView
         static void Main()
         {
             APIClient.Connect();
+            MailClient.Connect();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
